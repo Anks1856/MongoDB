c43c65f4113676c47e1984bfedc81cf9e48adc5e
Anks1856 <ankur.chovatiya1856@gmail.com>
2021-04-18 22:37:17 +0530

    add delete and update method added

--- Express/server.js
+++ Express/server.js
@@ -15,12 +15,52 @@ app.post('/register' , [express.json()], async (req , res)=>{
     res.send('You are register successfully!');
 });
 
+// add data into database
+
 app.post('/addStudent' , [express.json()] ,async (req , res)=>{
 
     let student = new model.Student(req.body);
     let result = await student.save();
     res.send('Student added successfully!');
 
+});
+
+
+//get the data from database
+
+app.get('/getStudent' , async(req , res)=>{
+    model.Student.find().select().exec((err , result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+});
+
+// update the data into database
+
+app.put('/studentUpdate', [express.json()] , async (req , res)=>{
+    let stuName = req.body.stuname;
+    let rollNo = req.body.rollNo;
+    console.log(rollNo);
+    model.Student.updateOne({StuName:stuName},[
+        {$set :{StuRollNo:rollNo}}
+    ]).exec((err , result)=>{
+        if(err) throw err;
+        res.send('Your data is succesfully updated');
+    });
+    
+});
+
+// delete the data from database
+
+app.delete('/userDelete' ,[express.json()] , async (req , res)=>{
+
+    let name = req.body.name;
+    model.User.deleteOne({name:name}).exec((err , result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send('your record succesfully deleted');
+    });
 });
 
 
